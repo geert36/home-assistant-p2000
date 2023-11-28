102260dec6ec5e50f43b5e0a0d9aa3a5639465d5
geert36 <77209691+geert36@users.noreply.github.com>
2023-11-28 19:35:27 +0100

    Update sensor.py

--- custom_components/p2000/sensor.py
+++ custom_components/p2000/sensor.py
@@ -4,7 +4,9 @@ import logging
 import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
-from homeassistant.const import (CONF_NAME)
+from homeassistant.const import (
+    CONF_NAME,
+    CONF_ICON)
 import homeassistant.helpers.config_validation as cv
 from .api import P2000Api
 
@@ -12,13 +14,16 @@ from .api import P2000Api
 _LOGGER = logging.getLogger(__name__)
  
 DEFAULT_NAME = "p2000"
+DEFAULT_ICON = "mdi:ambulance"
+
 CONF_GEMEENTEN = "gemeenten"
 CONF_CAPCODES = "capcodes"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Optional(CONF_GEMEENTEN): vol.All(cv.ensure_list, [cv.string]),
-    vol.Optional(CONF_CAPCODES): vol.All(cv.ensure_list, [cv.string])
+    vol.Optional(CONF_CAPCODES): vol.All(cv.ensure_list, [cv.string]),
+    vol.Optional(CONF_ICON, default=DEFAULT_ICON): cv.icon
 })
 
 
@@ -26,6 +31,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     """Setup the sensor platform."""
 
     name = config.get(CONF_NAME)
+    icon = config.get(CONF_ICON)
 
     filter = {
         "gemeenten": config.get(CONF_GEMEENTEN),
@@ -36,24 +42,30 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
     api = P2000Api()
 
-    add_entities([P2000Sensor(api, name, filter)])
+    add_entities([P2000Sensor(api, name, icon, filter)])
 
 
 class P2000Sensor(SensorEntity):
     """Representation of a Sensor."""
 
-    def __init__(self, api, name, filter):
+    def __init__(self, api, name, icon, filter):
         """Initialize the sensor."""
         self.api = api
         self.attributes = {}
         self.filter = filter
         self._name = name
+        self._icon = icon
         self._state = None
 
     @property
     def name(self):
         """Return the name of the sensor."""
         return self._name
+
+    @property
+    def icon(self):
+        """Return the icon of the sensor."""
+        return self._icon
 
     @property
     def state(self):
@@ -64,7 +76,6 @@ class P2000Sensor(SensorEntity):
     def extra_state_attributes(self):
         """Return the state attributes of the monitored installation."""
         attributes = self.attributes
-        attributes['icon'] = 'mdi:fire-truck'
         return attributes
 
     def update(self):
@@ -79,4 +90,3 @@ class P2000Sensor(SensorEntity):
 
         self.attributes = data
         self._state = data["id"]
-
