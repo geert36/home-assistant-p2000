56a4c94a350af3ad20a3920e47a31d007d89f376
geert36 <77209691+geert36@users.noreply.github.com>
2026-01-04 16:38:04 +0100

    Refactor sensor.py for improved configuration handling
    Refactor P2000 sensor configuration and setup.

--- custom_components/p2000/sensor.py
+++ custom_components/p2000/sensor.py
@@ -1,86 +1,108 @@
 import logging
 import voluptuous as vol
+from typing import Any, Dict
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
 from homeassistant.const import CONF_NAME, CONF_ICON
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
 
 from .api import P2000Api
+from .const import (
+    DEFAULT_NAME,
+    DEFAULT_ICON,
+    CONF_GEMEENTEN,
+    CONF_CAPCODES,
+    CONF_REGIOS,
+    CONF_DISCIPLINES,
+)
 
-"""Start the logger"""
 _LOGGER = logging.getLogger(__name__)
- 
-DEFAULT_NAME = "p2000"
 
-CONF_GEMEENTEN = "gemeenten"
-CONF_CAPCODES = "capcodes"
-CONF_DIENSTEN = "diensten"
-CONF_WOONPLAATSEN = "woonplaatsen"
-CONF_REGIOS = "regios"
-CONF_PRIO1 = "prio1"
-CONF_LIFE = "lifeliners"
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+        vol.Optional(CONF_ICON, default=DEFAULT_ICON): cv.icon,
+        vol.Optional(CONF_GEMEENTEN): vol.All(cv.ensure_list, [cv.string]),
+        vol.Optional(CONF_CAPCODES): vol.All(cv.ensure_list, [cv.string]),
+        vol.Optional(CONF_REGIOS): cv.string,
+        vol.Optional(CONF_DISCIPLINES): cv.string,
+    }
+)
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_ICON, default="mdi:fire-truck"): cv.icon,
-    vol.Optional(CONF_WOONPLAATSEN): vol.All(cv.ensure_list, [cv.string]),
-    vol.Optional(CONF_GEMEENTEN): vol.All(cv.ensure_list, [cv.string]),
-    vol.Optional(CONF_CAPCODES): vol.All(cv.ensure_list, [cv.string]),
-    vol.Optional(CONF_REGIOS): vol.All(cv.ensure_list, [cv.string]),
-    vol.Optional(CONF_DIENSTEN): vol.All(cv.ensure_list, [cv.string]),
-    vol.Optional(CONF_PRIO1, default=False): cv.boolean,
-    vol.Optional(CONF_LIFE, default=False): cv.boolean,    
-})
-
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Set up the sensor platform."""
-
+async def async_setup_platform(
+    hass: HomeAssistant,
+    config: Dict[str, Any],
+    async_add_entities: AddEntitiesCallback,
+    discovery_info=None,
+) -> None:
+    """Set up the P2000 sensor platform asynchronously."""
     name = config.get(CONF_NAME)
     icon = config.get(CONF_ICON)
-    
-    api_filter = {}
-        
-    # String / list properties
-    for prop in [
-        CONF_WOONPLAATSEN,
-        CONF_GEMEENTEN,
-        CONF_CAPCODES,
-        CONF_DIENSTEN,
-        CONF_REGIOS,
-    ]:
-        if prop in config:
-            api_filter[prop] = config[prop]
 
-    # Boolean properties
-    for prop in [CONF_PRIO1, CONF_LIFE]:
-        if config.get(prop):
-            api_filter[prop] = "1"
+    api_filter = {
+        "gemeenten": config.get(CONF_GEMEENTEN, []),
+        "capcodes": config.get(CONF_CAPCODES, []),
+        "regios": config.get(CONF_REGIOS, ""),
+        "disciplines": config.get(CONF_DISCIPLINES, ""),
+    }
 
-    api = P2000Api(hass)
+    _LOGGER.info("P2000 filter being used: %s", api_filter)
 
-    async_add_entities([
-        P2000Sensor(api, name, icon, api_filter)
-    ])
+    # Gebruik de Home Assistant aiohttp session
+    session = async_get_clientsession(hass)
+    api = P2000Api(session)
+
+    async_add_entities(
+        [P2000Sensor(api, name, icon, api_filter)],
+        update_before_add=True,
+    )
 
 class P2000Sensor(SensorEntity):
-    """Representation of a Sensor."""
+    """Representation of a P2000 Sensor."""
 
-    def __init__(self, api, name, icon, api_filter):
-        """Initialize the sensor."""
+    def __init__(
+        self,
+        api: P2000Api,
+        name: str,
+        icon: str,
+        api_filter: Dict[str, Any],
+    ):
         self.api = api
+        self.api_filter = api_filter
+
         self._attr_name = name
         self._attr_icon = icon
-        self.api_filter = api_filter
-     
-        self._attr_native_value = None
-        self._attr_extra_state_attributes = {}
+        self._attr_native_value: str | None = None
+        self._attr_extra_state_attributes: Dict[str, Any] = {}
 
-    async def async_update(self):
-        """Fetch new state data."""
-        data = await self.api.get_data(self.api_filter)
+        # Uniek ID (nodig voor entity registry)
+        self._attr_unique_id = f"p2000_{name.lower()}"
 
-        if not data:
-            return
+    async def async_update(self) -> None:
+        """Fetch new state data for the sensor."""
+        try:
+            _LOGGER.debug(
+                "Fetching P2000 data with filter: %s",
+                self.api_filter,
+            )
 
-        self._attr_extra_state_attributes = data
-        self._attr_native_value = data.get("melding")
+            data = await self.api.get_data(self.api_filter)
+
+            if not data:
+                self._attr_native_value = None
+                self._attr_extra_state_attributes = {}
+                return
+
+            self._attr_native_value = data.get("id")
+            self._attr_extra_state_attributes = data
+
+            if self._attr_native_value is None:
+                _LOGGER.warning("Received data does not contain an 'id'")
+
+        except Exception as err:
+            _LOGGER.error("Error updating P2000 sensor: %s", err)
+            self._attr_native_value = None
+            self._attr_extra_state_attributes = {}
