c5968fbf978edf54a2b711b488c40a0e45688ac7
geert36 <77209691+geert36@users.noreply.github.com>
2026-01-04 16:38:59 +0100

    Add constants for p2000 integration

--- custom_components/p2000/const.py
+++ custom_components/p2000/const.py
@@ -1,1 +1,9 @@
+DOMAIN = "p2000"
 
+DEFAULT_NAME = "p2000"
+DEFAULT_ICON = "mdi:ambulance"
+
+CONF_GEMEENTEN = "gemeenten"
+CONF_CAPCODES = "capcodes"
+CONF_REGIOS = "regios"
+CONF_DISCIPLINES = "disciplines"
